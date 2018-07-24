d8d8c53ea518f5c3a779bc96cad0aaf0081f7c34
Truong Chau Thy <truong_chau_thy@artc.a-star.edu.sg>
2018-07-24 17:12:13 +0800

    Fix merge

--- ws2812controller/animations/control.js
+++ ws2812controller/animations/control.js
@@ -1,19 +1,4 @@
 /*******************************
-<<<<<<< HEAD
-	animation control
-*******************************/
-	var common = require("./common.js");
-	var name = "control.js";
-
-	function control() {
-
-		this.Stop = function(args, strip) {
-			strip.SetStripColor(0);
-			strip.Mode = "STOP";
-			console.log("Stopped strip");
-		};
-	}
-=======
 animation control
 *******************************/
 var common = require("./common.js");
@@ -27,6 +12,5 @@ function control() {
     console.log("Stopped strip");
   };
 }
->>>>>>> e5ad7d12d7951be3d4432892f664ea090f0e65b7
 
 module.exports = new control();
